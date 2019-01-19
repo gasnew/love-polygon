7b7b9a66311531d17aab67515bca2810754f107e
Garrett Newman <garrett.s.newman@gmail.com>
2019-01-19 20:31:45 +0000

    wink

---
+++ src/renderer.js
@@ -0,0 +1,36 @@
+import * as THREE from 'three';
+
+function animate(props) {
+  requestAnimationFrame(() => animate(props));
+
+  const { mesh, scene, camera, renderer } = props;
+  mesh.rotation.x += 0.01;
+  mesh.rotation.y += 0.02;
+
+  renderer.render(scene, camera);
+}
+
+export default function getRendererElement() {
+  const camera = new THREE.PerspectiveCamera(
+    70,
+    window.innerWidth / window.innerHeight,
+    0.01,
+    10
+  );
+  camera.position.z = 1;
+
+  const scene = new THREE.Scene();
+
+  const geometry = new THREE.BoxGeometry(0.2, 0.2, 0.2);
+  const material = new THREE.MeshNormalMaterial();
+
+  const mesh = new THREE.Mesh(geometry, material);
+  scene.add(mesh);
+
+  const renderer = new THREE.WebGLRenderer({ antialias: true });
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  const props = { mesh, scene, camera, renderer };
+  animate(props);
+
+  return renderer.domElement;
+}
